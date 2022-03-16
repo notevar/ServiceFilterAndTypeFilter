85e06669e05e4d9b49f21daee8131e74d515fd5f
nico <Notevar>
2022-03-16 17:19:02 +0800

    添加项目文件。

---
+++ TestActionFilterAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApplication3
+{
+    public class TestActionFilterAttribute : ActionFilterAttribute
+    {
+        private readonly ILogger<TestActionFilterAttribute> _logger;
+
+        public TestActionFilterAttribute(ILogger<TestActionFilterAttribute> logger)//,string action
+        {
+            _logger = logger;
+            //this.ActionName = action;
+        }
+
+        public string ActionName { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation("do something.....");
+            base.OnActionExecuting(context);
+        }
+    }
+}
